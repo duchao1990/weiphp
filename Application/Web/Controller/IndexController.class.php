8b9b1d434db15ac0163b386b67efde00f16f1e77
company <384652412@qq.com>
2016-07-13 09:54:02 +0800

    20160713
    初始化

--- Application/Web/Controller/IndexController.class.php
+++ Application/Web/Controller/IndexController.class.php
@@ -33,4 +33,47 @@ class IndexController extends Controller {
     function enter(){
         $this->display();
     }
+
+    //根据添加表单的信息进入不同的页面
+    function addForm(){
+        if (IS_AJAX){
+            $templeData=array(
+                'temple'=>I('post.temple'),
+                'province'=>I('post.province'),
+                'city'=>I('post.city'),
+                'district'=>I('post.district'),
+                'authNum'=>date('YmdHis')
+            );
+            $salt=salt();
+            $templeModel=M('temple');
+            $templeId=$templeModel->add($templeData);
+            if ($templeId){
+                $userData=array(
+                    'mobile'=>I('post.mobile'),
+                    'master'=>I('post.master'),
+                    'templeid'=>$templeId,
+                    'salt'=>$salt,
+                    'pwd'=>createPwd(I('post.mobile'),$salt)
+                );
+                $masterid=M('master')->add($userData);
+                if ($masterid){
+                    $map['id']=intval($templeId);
+                    $res=$templeModel->where($map)->save(array('masterid'=>$masterid));
+                    if ($res){
+
+                        $this->success('登记成功,请上传认证资料',U('auth'));
+                    }else{
+                        $this->error('寺院更新失败,请联系客服');
+                    }
+                }else{
+                    $this->error('用户登记失败,请联系客服');
+                }
+            }else{
+                $this->error('寺院登记失败,请联系客服');
+            }
+        }else{
+            $this->display();
+        }
+
+    }
 }
